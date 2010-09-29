c5aeb3711a834edcd23d272dd1ff4470e017143a
centralniak <p.kilczuk@stylesheet.pl>
2010-09-29 16:35:43 +0200

    README update, version bump

--- README.rst
+++ README.rst
@@ -13,13 +13,14 @@ Installation
 
 1. Install requirements and put cmsplugin_gallery on your python path
 
+2. Add ``cmsplugin_gallery`` to your installed apps
+
 2. Run syncdb 
 
 3. Create directory for storing media files - files will be uploaded to MEDIA_ROOT + 'cmsplugin_gallery/images'.
    Make sure it is writable especially when running in embedded mode on production server. 
 
 4. Install jQueryTOOLS for overlay support using your favorite method
-   
    http://flowplayer.org/tools/download/index.html
 
 Usage
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='cmsplugin_gallery',
-    version='0.1.0',
+    version='0.1.1',
     author='Piotr Kilczuk',
     author_email='p.kilczuk@neumea.pl',
     url='http://github.com/centralniak',
     description = 'DjangoCMS image gallery plugin with drag&drop reordering in admin,' \
                   'support for thumbnails and jQueryTOOLS overlay.',
     packages = find_packages(),
-    provides = ['cmsplugin_gallery (0.1.0)',],
+    provides = ['cmsplugin_gallery (0.1.1)',],
     include_package_data=True,
-    requires = ['django-inline-ordering', 'easy-thumbnails']
+    requires = ['inline_ordering (>=0.1.1)', 'easy_thumbnails']
 )
