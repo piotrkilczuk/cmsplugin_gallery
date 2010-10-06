53a727001b2380666fd35d6e6c4c1d3fd41c7773
centralniak <p.kilczuk@stylesheet.pl>
2010-10-06 16:38:58 +0200

    Corrected requires statement, version bump

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='cmsplugin_gallery',
-    version='0.1.1',
+    version='0.1.2',
     author='Piotr Kilczuk',
     author_email='p.kilczuk@neumea.pl',
     url='http://github.com/centralniak',
     description = 'DjangoCMS image gallery plugin with drag&drop reordering in admin,' \
                   'support for thumbnails and jQueryTOOLS overlay.',
     packages = find_packages(),
-    provides = ['cmsplugin_gallery (0.1.1)',],
+    provides = ['cmsplugin_gallery (0.1.2)',],
     include_package_data=True,
-    requires = ['inline_ordering (>=0.1.1)', 'easy_thumbnails']
+    install_requires = ['django-inline-ordering>=0.1.1', 'easy-thumbnails',]
 )
