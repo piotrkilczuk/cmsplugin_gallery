6fcaddbb953e508e9cbfb64c031340ae855ab715
centralniak <p.kilczuk@stylesheet.pl>
2010-10-25 15:23:59 +0200

    Added support for unicode filenames, version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='cmsplugin_gallery',
-    version='0.1.6',
+    version='0.1.7',
     author='Piotr Kilczuk',
     author_email='p.kilczuk@neumea.pl',
     url='http://github.com/centralniak',
