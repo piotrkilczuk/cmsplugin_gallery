4e0e8bd082d84654bbc50e3abda8097ca05b8c52
centralniak <p.kilczuk@stylesheet.pl>
2012-02-04 11:53:28 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='cmsplugin_gallery',
-    version='0.4.9',
+    version='0.5.0',
     author='Piotr Kilczuk',
     author_email='p.kilczuk@neumea.pl',
     url='http://github.com/centralniak',
