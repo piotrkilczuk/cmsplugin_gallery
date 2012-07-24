1aa36f5031c7c54f0a375a3afb79ab980581cd20
centralniak <p.kilczuk@stylesheet.pl>
2012-07-24 21:15:48 +0200

    Add @shagi to AUTHORS, bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='cmsplugin_gallery',
-    version='0.5.1',
+    version='0.5.2',
     author='Piotr Kilczuk',
     author_email='p.kilczuk@neumea.pl',
     url='http://github.com/centralniak',
