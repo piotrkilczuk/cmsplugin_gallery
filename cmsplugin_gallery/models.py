7f36ea4da1e7fb0472b8a2cadcb22c912ca2523f
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-07-19 13:17:38 +0530

    add support for 1.8+ django

--- cmsplugin_gallery/models.py
+++ cmsplugin_gallery/models.py
@@ -7,6 +7,8 @@ from cms.utils import get_cms_setting
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from inline_ordering.models import Orderable
+from django.utils.deconstruct import deconstructible
+
 
 import utils
 
@@ -14,6 +16,18 @@ localdata = threading.local()
 localdata.TEMPLATE_CHOICES = utils.autodiscover_templates()
 TEMPLATE_CHOICES = localdata.TEMPLATE_CHOICES
 
+
+@deconstructible
+class UploadPath(object):
+
+    def __init__(self, sub_path):
+        self.path = sub_path
+
+    def __call__(self, instance, filename):
+        return "theming/%s/%s/%s" % (instance.generic_title, connection.schema_name, filename)
+
+
+get_upload_path = UploadPath('GalleryPlugin')
 
 class GalleryPlugin(CMSPlugin):
 
@@ -49,7 +63,7 @@ class Image(Orderable):
                 str(today.year), str(today.month), str(today.day), filename)
 
     gallery = models.ForeignKey(GalleryPlugin, verbose_name=_("Gallery"))
-    src = models.ImageField(_("Image file"), upload_to=get_media_path,
+    src = models.ImageField(_("Image file"), upload_to=get_upload_path,
                             height_field='src_height',
                             width_field='src_width')
     src_height = models.PositiveSmallIntegerField(_("Image height"), editable=False, null=True)
