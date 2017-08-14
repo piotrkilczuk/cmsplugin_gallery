c85b1202bf54e306c213121cddc3e70ebff6530a
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2017-08-14 23:48:07 +0530

    run tests for plugin's models

--- tests/tests_models.py
+++ tests/tests_models.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
-
-#from cmsplugin_gallery.models import GalleryPlugin, Image
+from cmsplugin_gallery.models import GalleryPlugin, Image
 
 
 class GalleryTestCase(TestCase):
 
     def setUp(self):
-        pass
+        self.gallery = GalleryPlugin.objects.create(
+            template='cmsplugin_gallery/gallery.html')
+        self.image = Image.objects.create(src='cms_page_media/110/123.jpg', gallery=self.gallery)
 
     def test_gallery_instance(self):
-        self.assertEqual(1, 1)
+        image_instance = Image.objects.get(src='cms_page_media/110/123.jpg')
+        self.assertEqual(image_instance.gallery.id, self.gallery.id)
