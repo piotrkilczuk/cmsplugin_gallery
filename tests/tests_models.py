65e069aa8f24ef8290e727989604390d7cb50e77
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2017-08-14 21:27:25 +0530

    add tox and proper tests

---
+++ tests/tests_models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+from cmsplugin_gallery.models import GalleryPlugin, Image
+
+
+class GalleryTestCase(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_gallery_instance(self):
+        self.assertEqual(1, 1)
