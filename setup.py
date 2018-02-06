312d6fd26eecfb7a16cebcf4b2908943d776ee56
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2018-02-06 11:14:46 +0530

    fix import issue

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='cmsplugin_gallery',
-    version='0.6.11',
+    version='0.6.12',
     author='Piotr Kilczuk',
     author_email='piotr@tymaszweb.pl',
     url='http://github.com/centralniak',
