827071c15305759e9055ca937cee47b55d20971b
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2018-03-14 23:42:03 +0530

    add slider back

--- cmsplugin_gallery/cms_plugins.py
+++ cmsplugin_gallery/cms_plugins.py
@@ -1,9 +1,10 @@
+from __future__ import absolute_import
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
 
-import admin
-import models
+from . import admin
+from . import models
 
 
 class CMSGalleryPlugin(CMSPluginBase):
@@ -15,8 +16,11 @@ class CMSGalleryPlugin(CMSPluginBase):
     render_template = 'cmsplugin_gallery/gallery.html'
 
     def render(self, context, instance, placeholder):
+        images = instance.image_set.all()
+        grouped_images = [images[i:i + 4] for i in xrange(0, len(images.values_list('src')), 4)]
         context.update({
                         'images': instance.image_set.all(),
+                        'grouped_images': grouped_images,
                         'gallery': instance,
                        })
         self.render_template = instance.template
