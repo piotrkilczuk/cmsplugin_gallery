fe36d81f2d8665ca699bd52af12f863fa6fcc907
Vinit Kumar <vinit.kumar@socialschools.nl>
2018-05-03 14:46:04 +0530

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='cmsplugin_gallery',
-    version='1.1.5',
+    version='1.1.6',
     author='Piotr Kilczuk',
     author_email='piotr@tymaszweb.pl',
     url='http://github.com/centralniak',
